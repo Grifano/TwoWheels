b8c5cec6a7eec6152c35bcd295d73f50212a6b70
Grifano <sorlenk@gmail.com>
2020-07-26 11:06:03 +0200

    make spoiler

--- js/main.js
+++ js/main.js
@@ -289,7 +289,7 @@ $(document).ready(function () {
 	$('.spoiler__title').click(function (event) {
 		if ($('.spoiler').hasClass('one')) { // .one - class for en/dis akordeon
 			$('.spoiler__title').not($(this)).removeClass('active');
-			$('.spoiler__text').not($(this).next()).slideUp(300);
+			$('.spoiler__item').not($(this).next()).slideUp(300);
 		}
 		$(this).toggleClass('active').next().slideToggle(300);
 	});
