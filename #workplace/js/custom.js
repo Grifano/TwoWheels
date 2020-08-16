dc444e4fab221e0aecba363a13fbb785757cdd50
Grifano <sorlenk@gmail.com>
2020-08-16 11:43:28 +0200

    make a filter

--- #workplace/js/custom.js
+++ #workplace/js/custom.js
@@ -98,22 +98,53 @@ mainblock();
 //****************************************
 
 // === Filtr & lighted active item ===
-$('.filter__link').click(function (event) {
-	var i = $(this).data('filter'); // $(this) - значит к только этому ('.filter__item').
-	if (!$(e.target).closest(".parent_block").length) {
-		$('.toggled_block').hide();
+filterSelection("all")
+function filterSelection(c) {
+	var x, i;
+	x = document.getElementsByClassName("product__column");
+	if (c == "all") c = "";
+	// Add the "show" class (display:block) to the filtered elements, and remove the "show" class from the elements that are not selected
+	for (i = 0; i < x.length; i++) {
+		w3RemoveClass(x[i], "show");
+		if (x[i].className.indexOf(c) > -1) w3AddClass(x[i], "show");
 	}
-	if (i == 1) { // если class .filter__item имеет data-filter="1" - значит...
-		$('.product__column').show(); // ...показать колонки.
-	} else { // иначе...
-		$('.product__column').hide(); // ...спрятать все колонки.
-		$('.product__column.f_' + i).show(); // но показать те которые имеют клас f_ + i
+}
+
+// Show filtered elements
+function w3AddClass(element, name) {
+	var i, arr1, arr2;
+	arr1 = element.className.split(" ");
+	arr2 = name.split(" ");
+	for (i = 0; i < arr2.length; i++) {
+		if (arr1.indexOf(arr2[i]) == -1) {
+			element.className += " " + arr2[i];
+		}
 	}
-	// Подвсветка активного фильтра
-	$('.filter__link').removeClass('active');
-	$(this).addClass('active');
-	return false;
-});
+}
+
+// Hide elements that are not selected
+function w3RemoveClass(element, name) {
+	var i, arr1, arr2;
+	arr1 = element.className.split(" ");
+	arr2 = name.split(" ");
+	for (i = 0; i < arr2.length; i++) {
+		while (arr1.indexOf(arr2[i]) > -1) {
+			arr1.splice(arr1.indexOf(arr2[i]), 1);
+		}
+	}
+	element.className = arr1.join(" ");
+}
+
+// Add active class to the current control button (highlight it)
+var btnContainer = document.getElementById("productFilterBtnContainer");
+var btns = btnContainer.getElementsByClassName("filter__link");
+for (var i = 0; i < btns.length; i++) {
+	btns[i].addEventListener("click", function () {
+		var current = document.getElementsByClassName("active");
+		current[0].className = current[0].className.replace(" active", "");
+		this.className += " active";
+	});
+}
 // === Filtr & lighted active item ===
 
 //****************************************
